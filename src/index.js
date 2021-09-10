d9cec4094280e758f55c00f3f95551e6a8b2e3ea
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-09-09 22:24:18 -0700

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import showError from './showError.js';
 const refresh = document.getElementById('refresh');
 const form = document.getElementById('form');
 const gameApi = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/GDUN7EKtNyCRpmru02cT/scores';
+const scoresContainer = document.getElementById('table');
 
 refresh.addEventListener('click', () => {
+  scoresContainer.innerHTML = '';
   getScores(gameApi).then((data) => showScores(data.result));
 });
 
